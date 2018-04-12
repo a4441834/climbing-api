2d774d0297a0ffad36c04256995d315ef2add4ab
Hugo Hendriks <hugohendriks@gmail.com>
2018-04-12 21:24:07 +0200

    changed climber

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO climber (id, firstname, lastname) VALUES (1, 'Hugo', 'Hendriks')
+INSERT INTO climber (id, firstname, lastname) VALUES (1, 'Adam', 'Ondra')
 INSERT INTO climber (id, firstname, lastname) VALUES (2, 'Wolfgang', 'Gullich')
 INSERT INTO climber (id, firstname, lastname) VALUES (3, 'Lynn', 'Hill')
